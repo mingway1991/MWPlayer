568b0de132a29044f7233ad63aa1925dedbe9e4e
shimingwei <shimingwei@lvmama.com>
2019-04-30 09:49:58 +0800

    Initial Commit

---
+++ EchoVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EchoVideo
+//
+//  Created by 石茗伟 on 2019/4/30.
+//  Copyright © 2019 聽風入髓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EchoVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EchoVideo
+//
+//  Created by 石茗伟 on 2019/4/30.
+//  Copyright © 2019 聽風入髓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
